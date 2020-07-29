c6a8b7eddf73941e7adc67d0d6732ae7ef9663ea
Michael Meienberg <mei@acs-ag.ch>
2020-07-29 13:27:41 +0200

    json getter

---
+++ JusiJSONHelper/IOBrokerJSONGet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JusiJSONHelper
+{
+    public class IOBrokerJSONGet
+    {
+        public string val { get; set; }
+        public string ack { get; set; }
+        public string from { get; set; }
+    }
+}
